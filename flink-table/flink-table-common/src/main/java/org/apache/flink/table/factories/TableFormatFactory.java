5abda39bfa8159823d14e8501ff7d1451124b266
Dawid Wysakowicz <dwysakowicz@apache.org>
2019-06-18 09:58:46 +0200

    [hotfix][table-common] Remove duplicated method TableFormatFactory#supportedProperties

--- flink-table/flink-table-common/src/main/java/org/apache/flink/table/factories/TableFormatFactory.java
+++ flink-table/flink-table-common/src/main/java/org/apache/flink/table/factories/TableFormatFactory.java
@@ -40,29 +40,4 @@ public interface TableFormatFactory<T> extends TableFactory {
 	 * supported properties.
 	 */
 	boolean supportsSchemaDerivation();
-
-	/**
-	 * List of format property keys that this factory can handle. This method will be used for
-	 * validation. If a property is passed that this factory cannot handle, an exception will be
-	 * thrown. The list must not contain the keys that are specified by the context.
-	 *
-	 * <p>Example format properties might be:
-	 *   - format.line-delimiter
-	 *   - format.ignore-parse-errors
-	 *   - format.fields.#.type
-	 *   - format.fields.#.name
-	 *
-	 * <p>If schema derivation is enabled, the list must include schema properties:
-	 *   - schema.#.name
-	 *   - schema.#.type
-	 *
-	 * <p>Note: All supported format properties must be prefixed with "format.". If schema derivation is
-	 * enabled, also properties with "schema." prefix can be used.
-	 *
-	 * <p>Use "#" to denote an array of values where "#" represents one or more digits. Property
-	 * versions like "format.property-version" must not be part of the supported properties.
-	 *
-	 * <p>See also {@link TableFactory#supportedProperties()} for more information.
-	 */
-	List<String> supportedProperties();
 }
