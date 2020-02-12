fc16444037454102a0bbd4d81c6ed0b2f47d04b8
Dawid Wysakowicz <dwysakowicz@apache.org>
2020-02-12 18:46:03 +0100

    [FLINK-16035][table-api-java-bridge] Updated StreamTableEnvironment.java to use expression when converting DataStream to Table

--- flink-table/flink-table-api-java-bridge/src/main/java/org/apache/flink/table/api/java/internal/StreamTableEnvironmentImpl.java
+++ flink-table/flink-table-api-java-bridge/src/main/java/org/apache/flink/table/api/java/internal/StreamTableEnvironmentImpl.java
@@ -65,6 +65,7 @@ import org.apache.flink.table.types.utils.TypeConversions;
 import org.apache.flink.table.typeutils.FieldInfoUtils;
 
 import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -212,9 +213,14 @@ public final class StreamTableEnvironmentImpl extends TableEnvironmentImpl imple
 	@Override
 	public <T> Table fromDataStream(DataStream<T> dataStream, String fields) {
 		List<Expression> expressions = ExpressionParser.parseExpressionList(fields);
+		return fromDataStream(dataStream, expressions.toArray(new Expression[0]));
+	}
+
+	@Override
+	public <T> Table fromDataStream(DataStream<T> dataStream, Expression... fields) {
 		JavaDataStreamQueryOperation<T> queryOperation = asQueryOperation(
 			dataStream,
-			Optional.of(expressions));
+			Optional.of(Arrays.asList(fields)));
 
 		return createTable(queryOperation);
 	}
@@ -236,6 +242,14 @@ public final class StreamTableEnvironmentImpl extends TableEnvironmentImpl imple
 
 	@Override
 	public <T> void createTemporaryView(String path, DataStream<T> dataStream, String fields) {
+		createTemporaryView(path, fromDataStream(dataStream, fields));
+	}
+
+	@Override
+	public <T> void createTemporaryView(
+			String path,
+			DataStream<T> dataStream,
+			Expression... fields) {
 		createTemporaryView(path, fromDataStream(dataStream, fields));
 	}
 
